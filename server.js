9ddbf5f8ac09a139357bd13e70338876a3cbcf68
mgmaresca <mariagarciamaresca@gmail.com>
2014-09-24 19:03:40 +0200

    Uso de sockets

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+
+
 var express = require('express'),
 	path 	= require('path'),
 	http 	= require('http'),
+	io		= require('socket.io'),
 	book 	= require('./routes/books');
 
 var app = express();
@@ -12,12 +15,39 @@ app.configure(function () {
 	app.use(express.static(path.join(__dirname, 'public')));
 });
 
+var server = http.createServer(app);
+io = io.listen(server);
+
+io.configure(function () {
+	io.set('authorization', function (handshakeData, callback) {
+		if (handshakeData.xdomain) {
+			callback('Cross-domain connections are not allowed');
+		} else {
+			callback(null, true);
+		}
+	});
+});
+
+server.listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+});
+
 app.get('/books', book.findAll);
 app.get('/books/:id', book.findById);
 app.post('/books', book.addBook);
 app.put('/books/:id', book.updateBook);
 app.delete('/books/:id', book.deleteBook);
 
-http.createServer(app).listen(app.get('port'), function () {
-	console.log("Express server listening on port " + app.get('port'));
+io.sockets.on('connection', function (socket) {
+	socket.on('message', function (message) {
+		console.log("Got message: " + message);
+		ip = socket.handshake.address.address;
+		url = message;
+		io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length, 'ip': '***.***.***.' + ip.substring(ip.lastIndexOf('.') + 1), 'url': url, 'xdomain': socket.handshake.xdomain, 'timestamp': new Date()});
+});
+
+socket.on('disconnect', function () {
+		console.log("Socket disconnected");
+		io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length});
+	});
 });
